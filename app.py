cd6aeda23323a4ede6c4fcc6f8f893273d8ae055
haroyan888 <haroyan3020@gmail.com>
2023-05-15 15:41:42 +0900

    DEBUG変数の追加とスタイル変更

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from route.route import route
+import os
+
+debug = os.environ.get("DEBUG")
+print(debug)
 
 app = Flask(__name__, static_url_path='/', static_folder='./static')
 app.register_blueprint(route)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=debug, host='0.0.0.0')
