ae75b66ad9ed71fef6f4b22150addc7523d8246e
haroyan888 <haroyan3020@gmail.com>
2023-05-15 12:02:04 +0900

    仮想環境によるエラーの修正

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from route.route import route
 
-app = Flask(__name__, static_folder="static")
+app = Flask(__name__, static_url_path='/', static_folder='./static')
 app.register_blueprint(route)
 
 if __name__ == '__main__':
